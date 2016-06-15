3c7c6deb3d3309bcd025896b9e90364a4de41a0b
Leonhard Künzler <Leonhard Knzler>
2016-06-16 00:08:17 +0200

    cleanup code, add doc, add version codes

--- app/src/main/java/io/kuenzler/android/stayawake/Xposed.java
+++ app/src/main/java/io/kuenzler/android/stayawake/Xposed.java
@@ -24,20 +24,18 @@ public class Xposed implements IXposedHookZygoteInit, IXposedHookLoadPackage {
     private static boolean flagKeepScreenOn;
 
 
-
     @Override
     public void initZygote(StartupParam param) throws Throwable {
-        //set current activity
+
+        //set current activity for later reference
         findAndHookMethod(android.app.Instrumentation.class, "newActivity", ClassLoader.class, String.class, Intent.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 currentActivity = (Activity) param.getResult();
-                //mCon = currentActivity.getApplicationContext();
-                // Toast.makeText(currentActivity, "hooked activity " + currentActivity.getPackageName(), Toast.LENGTH_SHORT).show();
-                //XposedBridge.log("hooked activity " + currentActivity.getPackageName());
             }
         });
 
+        //reset the flag on screen rotation -> onConfigurationChanged(Configuration config)
         findAndHookMethod(Activity.class, "onConfigurationChanged", Configuration.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
@@ -47,42 +45,43 @@ public class Xposed implements IXposedHookZygoteInit, IXposedHookLoadPackage {
             }
         });
 
+        //listen for KeyDown events
         findAndHookMethod(Activity.class, "onKeyDown", int.class, KeyEvent.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("KeyDown detected - " + currentActivity.getPackageName());
 
-                XposedBridge.log("KeyDown consumed");
+                //check param and set keyCode
                 if (!(param.args[0] instanceof Integer)) {
                     param.setResult(false);
                 }
                 int keyCode = (int) param.args[0];
+
                 if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+                    //volume down: set flag FLAG_KEEP_SCREEN_ON
                     param.setResult(true);
                     XposedBridge.log("key down consumed, screen should stay on");
                     setFlagKeepScreenOn(true);
-                    //View contentView = currentActivity.findViewById(android.R.id.content);
-                    //currentActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                    //currentActivity.setContentView(contentView);
-                    //currentActivity.recreate();
-                    //currentActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
                 } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+                    //volume up: clear flag FLAG_KEEP_SCREEN_ON
                     param.setResult(true);
                     XposedBridge.log("key up consumed, screen should stay off");
                     setFlagKeepScreenOn(false);
-                    //View contentView = currentActivity.findViewById(android.R.id.content);
-                    //currentActivity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                    //currentActivity.setContentView(contentView);
-                    //currentActivity.recreate();
-                    //currentActivity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                } else if (keyCode == KeyEvent.KEYCODE_BACK) {
+                } else if (keyCode == KeyEvent.KEYCODE_BACK && currentActivity.getPackageName().contains("io.kuenzler.android.stayawake")) {
+                    //TODO: debug msg
                     param.setResult(true);
-                    Toast.makeText(currentActivity, "KEEP_SCREEN_ON is " + getFlagKeepScreenOn(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(currentActivity, "KEEP_SCREEN_ON is " + isFlagKeepScreenOn(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    public boolean getFlagKeepScreenOn() {
+    /**
+     * Returns whether the FLAG_KEEP_SCREEN_ON flag is set in WindowManager
+     *
+     * @return true if set, false if not set
+     */
+    public boolean isFlagKeepScreenOn() {
         int flags, flag;
         flags = currentActivity.getWindow().getAttributes().flags;
         flag = flags & WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
@@ -93,6 +92,12 @@ public class Xposed implements IXposedHookZygoteInit, IXposedHookLoadPackage {
         }
     }
 
+    /**
+     * Sets the FLAG_KEEP_SCREEN_ON flag if given parameter is true, clears otherwise
+     *
+     * @param keepScreenOn set flag if true, clear if false
+     * @return true if flag is set, flase if not set
+     */
     private boolean setFlagKeepScreenOn(boolean keepScreenOn) {
         flagKeepScreenOn = keepScreenOn;
         if (keepScreenOn) {
@@ -100,13 +105,15 @@ public class Xposed implements IXposedHookZygoteInit, IXposedHookLoadPackage {
         } else {
             currentActivity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         }
-        Toast.makeText(currentActivity, "KEEP_SCREEN_ON is " + getFlagKeepScreenOn(), Toast.LENGTH_SHORT).show();
-        return getFlagKeepScreenOn();
+        //TODO: debug msg
+        Toast.makeText(currentActivity, "KEEP_SCREEN_ON is " + isFlagKeepScreenOn(), Toast.LENGTH_SHORT).show();
+        return isFlagKeepScreenOn();
     }
 
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        //check if module is enabled, used in ui
         if (lpparam.packageName.equals("io.kuenzler.android.stayawake")) {
             findAndHookMethod("io.kuenzler.android.stayawake.MainActivity", lpparam.classLoader, "isModuleActive", new XC_MethodHook() {
                 @Override
