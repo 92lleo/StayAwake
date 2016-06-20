e6491679672088f50f82e3469a2a89e9280ed199
Leonhard Künzler <Leonhard Knzler>
2016-06-20 18:50:17 +0200

    add error message for when module is not enabled

--- app/src/main/java/io/kuenzler/android/stayawake/MainActivity.java
+++ app/src/main/java/io/kuenzler/android/stayawake/MainActivity.java
@@ -1,5 +1,7 @@
 package io.kuenzler.android.stayawake;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +14,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (!isModuleActive()) {
+            xposedAlert();
+        } else {
+            Toast.makeText(this, "[Debug] Xposed module is active", Toast.LENGTH_SHORT).show();
+        }
         Log.i("StayAwake", String.valueOf(isModuleActive()));
-        Toast.makeText(this, String.valueOf(isModuleActive()), Toast.LENGTH_SHORT).show();
+
     }
 
-    public boolean isModuleActive(){
+    private void xposedAlert() {
+        new AlertDialog.Builder(this)
+                .setTitle("Xposed Error")
+                .setMessage("This Xposed module is not activated or Xposed is not installed. " +
+                        "You can't use this app without Xposed.\n\nDo you want to close it?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    public boolean isModuleActive() {
         return false;
     }
 }
